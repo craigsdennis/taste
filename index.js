1afd759d14ce006f7a425ddcecb2d8d0be8a0c31
craigsdennis <craig@teamtreehouse.com>
2017-08-24 00:27:24 -0700

    Adds new es6 tests

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const importsFlavor = require('./flavor_profiles/javascript/imports.json');
 // EXAMPLE USAGE
 const flavors = [es6Flavor, importsFlavor];
 // Get a known module
-const root = path.join(process.cwd(), 'node_modules', 'glob');
-const results = processor.taste(root, flavors);
+//const root = path.join(process.cwd(), 'node_modules', 'glob');
+const results = processor.taste(process.cwd(), flavors);
 console.log(JSON.stringify(results, null, 2));
--- test/samples/es6-sample.js
+++ test/samples/es6-sample.js
@@ -13,3 +13,35 @@ function usingRest(...theseAreRestArgs) {
 }
 
 setTimeout(() => console.log("Hello"), 1000);
+
+function holdItNow() {
+    return new Promise((resolve, reject) => {
+        resolve("Hit it!");
+    })
+}
+
+class Yep {
+    aMethod() {
+        return 42;
+    }
+}
+
+holdItNow.then(console.log).catch(err => 'me if you can');
+
+const collection1 = new Map();
+const collection2 = new Set();
+const collection3 = new WeakMap();
+const collection4 = new WeakSet();
+
+function* letters(word) {
+    for (let letter of word) {
+        yield letter;
+        console.log("Whee");
+    }
+}
+
+for (let letter of letters("antidisestablishmentantarianism")) {
+    console.log(letter);
+}
+
+
